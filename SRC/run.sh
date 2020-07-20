1d789fa5a5c98d7c491f8a13819033b7ac19ee38
ssmaximilian <41385813+ssmaximilian@users.noreply.github.com>
2020-07-20 10:51:15 -0400

    Add files via upload

---
+++ SRC/run.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+import os
+os.system(command)
+
+
+
+python3.8 ./SRC/InsightCodingChallenge.py ./input/dataIn.txt
+python3.8 ./SRC/InsightCodingChallenge.py ./output/dataOut.txt
+
+
+python3.8 ./SRC/InsightCodingChallenge.py ./input/dataIn.csv
+python3.8 ./SRC/InsightCodingChallenge.py ./output/dataOut.txt
