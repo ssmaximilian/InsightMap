f49442c30ee62e50c016b7ddcb2b6584ea0048fa
ssmaximilian <41385813+ssmaximilian@users.noreply.github.com>
2020-07-20 10:47:54 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,3 +8,6 @@ os.system(command)
 python3.8 ./SRC/InsightCodingChallenge.py ./input/dataIn.txt
 python3.8 ./SRC/InsightCodingChallenge.py ./output/dataOut.txt
 
+
+python3.8 ./SRC/InsightCodingChallenge.py ./input/dataIn.csv
+python3.8 ./SRC/InsightCodingChallenge.py ./output/dataOut.txt
